505bacef3c05eeead5f621f6e7bb8c49ca4d2f2a
Rainy <browngres@outlook.com>
2024-08-27 21:40:37 +0800

    添加 辅助 rust 验证结果的部分

--- tig/algo/dollop/help_rust.py
+++ tig/algo/dollop/help_rust.py
@@ -1,31 +1,25 @@
 import math
-from random import random
 
+from tig.algo.dp_1d import knapsack_dp_comp
 from tig.algo.dp_1d_selected import knapsack_dp_select
-from tig.algo.generate_instance import RngArray
 
 if __name__ == '__main__':
-    num_items = 100
-    better_than_baseline = 10
-    # seeds = [135] * 8  # 60 & 135
-    seeds = [random()] * 8
-    # 生成案例
-    rngs = RngArray(seeds)
-    rng = rngs.get_mut()
-    n = num_items
-    weights = [rngs.get_mut().randint(1, 49) for _ in range(n)]
-    values = [rngs.get_mut().randint(1, 49) for _ in range(n)]
-    capacity = sum(weights) // 2
-    print("weight：", weights)
-    print("value：", values)
-    print("背包容量：", capacity)
+    # rust   seed 666   n 60
+    weights = [10, 10, 43, 10, 15, 11, 18, 10, 41, 10, 17, 10, 12, 10, 27, 43, 43, 15, 43, 18, 15, 15, 18, 18, 41, 43, 41,
+             15, 41, 18, 17, 43, 17, 15, 17, 18, 12, 43, 12, 15, 12, 18, 27, 17, 17, 18, 22, 41, 41, 17, 41, 12, 17, 17,
+             12, 12, 27, 18, 48, 27]
+    values = [22, 17, 44, 18, 40, 17, 39, 27, 48, 18, 20, 22, 48, 22, 44, 40, 48, 44, 39, 17, 49, 43, 27, 44, 20, 49, 15,
+            27, 40, 40, 39, 18, 23, 39, 22, 23, 20, 25, 17, 25, 18, 31, 22, 31, 48, 20, 30, 48, 49, 18, 49, 41, 44, 3,
+            17, 23, 23, 25, 44, 3]
 
-    #  贪心算法给出一个值
+    n = len(weights)
+    capacity = sum(weights) // 2
+
     sorted_v_to_w_ratio = list(range(n))
     sorted_v_to_w_ratio.sort(key=lambda x: values[x] / weights[x], reverse=True)
     print("价值密度排序：", sorted_v_to_w_ratio)
+
     greedy_value = 0
-    greedy_selected = []
     limit1 = 0.0
     limit1_flag = True
     limit2 = 0.0
@@ -36,8 +30,6 @@ if __name__ == '__main__':
         item = sorted_v_to_w_ratio[i]
         if weights[item] > remain:
             # 拿不下
-            print(f"出现拿不下{item}，当前贪心拿了 {len(greedy_selected)} 个, 总重：{capacity-remain}")
-            print(f"这个价值密度为： ", values[item] / weights[item])
             if limit1_flag and (i > 0):
                 # 第一次出现拿不下，算出最优解的理论上限
                 # 根据前一次猜测最大值
@@ -58,21 +50,19 @@ if __name__ == '__main__':
         limit2_flag = 0
         greedy_value += values[item]
         remain -= weights[item]
-        greedy_selected.append(item)
 
     print("limit1：", limit1)
     print("limit2：", limit2)
     print("预测最优解：", math.ceil(0.62*limit1 + 0.38*limit2))  # 乐观向上
-    # 尽量估算高一点，避免错过有解的情况
-    # print("预测最优解：", round(0.5 * (limit1 + limit2)))
-    # print("greedy selected：", greedy_selected)
-    greedy_selected.sort()
-    # print("greedy selected sorted：", greedy_selected)
-    # print("greedy selected 数量：", len(greedy_selected))
     print("贪心基准线：", greedy_value)
+
+
+    # 一维 dp
+    res4 = knapsack_dp_comp(weights, values, capacity)
+    print("一维 dp：", res4)
 
     # 一维 dp 带选择
     res5, res5_selected = knapsack_dp_select(weights, values, capacity)
     print("一维 dp 带选择的结果：", res5)
-    # print("一维 dp 带选择的挑选：", res5_selected)
-    # print("一维 dp 带选择的挑选个数：", len(res5_selected))
+    print("一维 dp 带选择的挑选：", res5_selected)
+    print("一维 dp 带选择的挑选个数：", len(res5_selected))
